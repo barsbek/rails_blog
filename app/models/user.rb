7c3908d0ace284801c6717df48d2e7fc721e9c5c
toshiro <mak.bekmamat@gmail.com>
2017-09-28 07:48:51 +0500

    remove avatar's presence validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   has_attached_file :avatar,
     styles: {medium: "300x300>", small: "100x100>"}
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
-  validates_attachment_presence :avatar
   validates_presence_of :login, :name, :email
   validates :email, email: true, uniqueness: true
   validates :login, length: {minimum: 2}

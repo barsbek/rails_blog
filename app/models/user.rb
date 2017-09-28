389f1aba23824f4e791c11e54f0e6636481f0c55
toshiro <mak.bekmamat@gmail.com>
2017-09-28 05:21:46 +0500

    validate some of user's attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_attached_file :avatar,
     styles: {medium: "300x300>", small: "100x100>"}
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  validates_attachment_presence :avatar
+  validates_presence_of :login, :name, :email
+  validates :email, email: true, uniqueness: true
+  validates :login, length: {minimum: 2}
 end

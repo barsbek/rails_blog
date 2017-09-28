c3200424665c10e72eefa807e1ab7b627cc43cda
toshiro <mak.bekmamat@gmail.com>
2017-09-29 00:30:29 +0500

    system test user creation

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -1,9 +1,24 @@
 require "application_system_test_case"
 
 class UsersTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit users_url
-  #
-  #   assert_selector "h1", text: "User"
-  # end
+  def setup
+    @params = {
+      name: 'new name',
+      email: 'new@email.com',
+      login: 'new long',
+      password: 'new password',
+      password_confirmation: 'new password'
+    }
+  end
+
+  test "creating a user" do
+    visit new_user_url
+    @params.each do |param, value|
+      fill_in "user_#{param}", with: value
+    end
+
+    click_on "Create User"
+
+    assert_text "User was successfully created"
+  end
 end

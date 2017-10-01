cddeda63296f9ad9940fe9cfe7e1c72d3e8e2390
toshiro <mak.bekmamat@gmail.com>
2017-10-02 01:45:50 +0500

    create log in for admin

---
+++ app/controllers/admin/base_controller.rb
@@ -0,0 +1,36 @@
+class Admin::BaseController < ApplicationController
+  helper_method :admin_user  
+
+  before_action :require_login, except: [:log_in, :authenticate]
+  layout "admin"
+
+  def index
+  end
+
+  def log_in
+  end
+
+  def authenticate
+    admin = Admin.find_by_login(params[:login])
+    if admin && admin.authenticate(params[:password])
+      session[:admin_id] = admin.id
+      redirect_to admin_root_path
+    else
+      redirect_to admin_log_in_path
+    end
+  end
+
+  def log_out
+    session[:admin_id] = nil
+    redirect_to admin_log_in_path
+  end
+
+  def admin_user
+    Admin.find(session[:admin_id]) if session[:admin_id]
+  end
+
+  private
+    def require_login
+      redirect_to admin_log_in_path unless admin_user
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+  root "articles#index"
   resources :users
   resources :articles
   get "log_in" => "sessions#new"
   get "log_out" => "sessions#destroy"
   post "log_in" => "sessions#create"
   get "sessions" => "sessions#new"
+
+  namespace :admin do
+    root "base#index"
+    get "log_out", to: "base#log_out"
+    get "log_in", to: "base#log_in"
+    post "log_in", to: "base#authenticate"
+  end
 end
